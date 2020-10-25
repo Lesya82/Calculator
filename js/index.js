7c7b9c81b1ad5849e8095ad735d8606786a6d496
Lesya82 <lesya.postolaki@gmail.com>
2020-10-25 10:04:18 +0000

    Comment

---
+++ js/index.js
@@ -0,0 +1,48 @@
+function add(){
+    var numberOne;
+    var numberTwo;
+numberOne = document.getElementById("nOne").value;
+numberOne = parseInt(numberOne);
+numberTwo = document.getElementById("nTwo").value;
+numberTwo = parseInt(numberTwo);
+    result = numberOne  + numberTwo;
+    document.getElementById("out").innerHTML = result;
+}
+
+function subtract(){
+    let numberOne;
+    let numberTwo;
+numberOne = document.getElementById("nOne").value;
+numberOne = parseInt(numberOne);
+numberTwo = document.getElementById("nTwo").value;
+numberTwo = parseInt(numberTwo);
+    result = numberOne - numberTwo;
+    document.getElementById("out").innerHTML = result;
+}
+
+function multiply(){
+    let numberOne;
+    let numberTwo;
+numberOne = document.getElementById("nOne").value;
+numberOne = parseInt(numberOne);
+numberTwo = document.getElementById("nTwo").value;
+numberTwo = parseInt(numberTwo);
+    result = numberOne * numberTwo;
+    document.getElementById('out').innerHTML = result;
+}
+
+function division(){
+    let numberOne;
+    let numberTwo;
+numberOne = document.getElementById("nOne").value;
+numberOne = parseInt(numberOne);
+numberTwo = document.getElementById("nTwo").value;
+numberTwo = parseInt(numberTwo);
+if(numberTwo !== 0){
+result = numberOne / numberTwo;
+    document.getElementById("out").innerHTML = result;
+} else {
+    document.getElementById("out").innerHTML = "Division by 0";
+}
+}
+
